c562f4c20898649fa5eef0aeeed516b9d8c3fc9b
sundroppity <jjohnst7@ashland.edu>
2018-10-11 14:56:14 -0400

    Working to really refine mechanics of the convert process. Very early in the stage of that development for sure.

---
+++ js/convert.js
@@ -0,0 +1,8 @@
+// We need to establish some type of conversion factor, essentially, objects (kind of)
+/* Something like this, if possible:
+squareFeet {
+conversionFactor: 4050.15;
+smallerLarger: boolean;
+}
+ */
+
